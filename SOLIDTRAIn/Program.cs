26406fd15750adfcb287595c8c8d29c869a065c7
whooaaapppp <jwarain@me.com>
2019-12-15 21:23:59 -0500

    initial commit

---
+++ SOLIDTRAIn/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SOLIDTRAIn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
